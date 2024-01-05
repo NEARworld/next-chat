32bbb5ff26a4e4fceb38f84f6ebd33fd3a9fac04
nearworld <artemismars2@outlook.com>
2024-01-05 11:36:09 +0900

    define Input component props

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,3 +1,26 @@
-export default function Input() {
-  return <input />;
-}
+import { FC } from 'react';
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+
+type Props = {
+  label: string;
+  id: string;
+  type?: string;
+  required?: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+};
+
+const Input: FC<Props> = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
+  return <div></div>;
+};
+
+export default Input;
