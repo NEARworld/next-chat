18fa5f166fb14079d0343a694d88c2ca3e22599f
nearworld <artemismars2@outlook.com>
2024-01-05 20:19:32 +0900

    remove e.preventDefault() method in onSubmit function

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -30,7 +30,6 @@ export default function AuthForm() {
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (e) => {
-    e.preventDefault();
     setIsLoading(true);
     if (variant === 'REGISTER') {
     } // axios register
