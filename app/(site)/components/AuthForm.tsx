c622a4161479874db37cd7b7389754c0ec892f99
nearworld <artemismars2@outlook.com>
2024-01-05 11:20:32 +0900

    AuthForm business logic skeleton

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,3 +1,43 @@
+'use client';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
 export default function AuthForm() {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') setVariant('REGISTER');
+    else setVariant('LOGIN');
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    if (variant === 'REGISTER') {
+    } // axios register
+    if (variant === 'LOGIN') {
+      // NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
   return <div></div>;
 }
