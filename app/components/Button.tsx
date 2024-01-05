40a7324f5b090bfc8a837622cad83ca79d005ecf
nearworld <artemismars2@outlook.com>
2024-01-05 19:43:47 +0900

    style Button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { FC, PropsWithChildren } from 'react';
 
 type Props = {
@@ -18,7 +19,35 @@ const Button: FC<PropsWithChildren<Props>> = ({
   disabled,
   children,
 }) => {
-  return <button>button</button>;
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        `
+        flex
+        justify-center
+        rounded-md
+        px-3
+        py-2
+        text-sm
+        font-semibold
+        focus-visible:outline-2
+        focus-visible:outline-offset-2
+      `,
+        disabled && 'opacity-50 cursor-default',
+        fullWidth && 'w-full',
+        secondary ? 'text-gray-900' : 'text-white',
+        danger && 'bg-rose-500 hover:bg-600 focus:outline-rose-600',
+        !secondary &&
+          !danger &&
+          'bg-sky-500 hover:bg-sky-600 focus:outline-sky-600'
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
