31917e0c61ab9ab4833332fb2b66883b9ecea9b7
nearworld <artemismars2@outlook.com>
2024-01-06 10:53:11 +0900

    post request to register api

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,6 +5,7 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -29,10 +30,11 @@ export default function AuthForm() {
     },
   });
 
-  const onSubmit: SubmitHandler<FieldValues> = (e) => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant === 'REGISTER') {
-    } // axios register
+      axios.post('/api/register', data);
+    }
     if (variant === 'LOGIN') {
       // NextAuth SignIn
     }
