974063e946b93d0639e5bc4b491fb256fa193db2
nearworld <artemismars2@outlook.com>
2024-01-07 23:16:38 +0900

    a helper to get a current user

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
 import { FC, PropsWithChildren } from 'react';
 import DesktopSidebar from './DesktopSidebar';
 import MobileFooter from './MobileFooter';
+import getCurrentUser from '@/app/actions/getCurrentUser';
 
-const Sidebar: FC<PropsWithChildren> = ({ children }) => {
+const Sidebar: FC<PropsWithChildren> = async ({ children }) => {
+  const currentUser = await getCurrentUser();
   return (
     <div>
       <DesktopSidebar />
