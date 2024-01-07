76c505db3f38755cb37d5ce4ffc50c2d68f73609
nearworld <artemismars2@outlook.com>
2024-01-08 00:05:54 +0900

    render UserList on layout at /users path

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,16 @@
 import { FC, PropsWithChildren } from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
+import UserList from './UserList';
 
 const layout: FC<PropsWithChildren> = async ({ children }) => {
+  const users = await getUsers();
   return (
     <Sidebar>
-      <div className='h-full'>{children}</div>
+      <div className='h-full'>
+        <UserList users={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
