b8315a667840518571492f46acb54393f5cbe767
nearworld <artemismars2@outlook.com>
2024-01-07 21:50:02 +0900

    render mobile bottom navigation

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
 import DesktopSidebar from './DesktopSidebar';
+import MobileFooter from './MobileFooter';
 
 const Sidebar: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <DesktopSidebar />
+      <MobileFooter />
       <main className='lg:pl-20 h-full '>{children}</main>
     </div>
   );
