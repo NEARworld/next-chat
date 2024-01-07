11659870f39250fa94a257ce6821c3336b445733
nearworld <artemismars2@outlook.com>
2024-01-08 00:23:53 +0900

    UserBox css

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { User } from '@prisma/client';
 import { FC } from 'react';
 
@@ -6,7 +7,24 @@ type Props = {
 };
 
 const UserBox: FC<Props> = ({ user }) => {
-  return <div>user</div>;
+  return (
+    <div
+      className='
+        w-full 
+        relative 
+        flex 
+        items-center 
+        space-x-3 
+        bg-white 
+        p-3 
+        hover:bg-neutral-100 
+        rounded-lg 
+        transition 
+        cursor-pointer '
+    >
+      {/* <Avatar user={user} /> */}
+    </div>
+  );
 };
 
 export default UserBox;
